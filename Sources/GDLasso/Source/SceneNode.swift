a2f377ca2aaa0effa805fd54ad1cc2da624d7f19
joey <joey.nelson@ww.com>
2024-05-12 08:00:22 -0400

    fleshed out scenenode

--- Sources/GDLasso/Source/SceneNode.swift
+++ Sources/GDLasso/Source/SceneNode.swift
@@ -9,5 +9,26 @@
 import Foundation
 
 public protocol SceneNode {
-    associatedtype Store: AbstractNodeStore
+    
+    associatedtype NodeState
+    
+    associatedtype NodeAction
+    
+    typealias NodeStore = AnyNodeStore<NodeState, NodeAction>
+    
+    var store: NodeStore { get }
+    
+    func set(store: NodeStore)
+}
+
+extension SceneNode {
+    
+    public var state: NodeState {
+        return store.state
+    }
+    
+    public func dispatchAction(_ action: NodeAction) {
+        store.dispatchAction(action)
+    }
+    
 }
