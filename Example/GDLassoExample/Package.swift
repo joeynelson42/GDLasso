9c8e13d54ad4aebbe513a926294d4f6b13948f7f
joey <joey.nelson@ww.com>
2024-06-16 15:41:52 -0400

    updated example package branch

--- Example/GDLassoExample/Package.swift
+++ Example/GDLassoExample/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main"),
-        .package(url: "https://github.com/joeynelson42/GDLasso", branch: "origin/swiftGodot")
+        .package(url: "https://github.com/joeynelson42/GDLasso", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
