eca3501be03a6d43021760f8776e5fdeb6901d1c
Joey <jnelson@evluma.com>
2024-10-11 10:41:29 +0900

    updated SwiftGodot version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["GDLasso"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main")
+        .package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "0.45")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
